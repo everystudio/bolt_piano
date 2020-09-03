d142fc21e3e0f1b3eebbc7306eda5d370a566a8c
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-09-03 14:20:35 +0900

    ファイルの追加

---
+++ Assets/ButtonCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCheck : MonoBehaviour
+{
+    public List<Image> white_key;
+
+    public void Check()
+    {
+        white_key[0].color = Color.red;
+        white_key[2].color = Color.red;
+        white_key[4].color = Color.red;
+    }
+
+    public void Test()
+    {
+        Debug.Log("test");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
